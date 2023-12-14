0a5797b98e434adb0db442e9fd9efc5f5914e194
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-12-14 19:15:47 +0100

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <nav>Hallo!</nav>
+      <h1 className="text-3xl font-bold underline">Hallo!</h1>
       <Routes>
         <Route path="portfolio/" element={<Home />} />
         <Route path="portfolio/about" element={<About />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
