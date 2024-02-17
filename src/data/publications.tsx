093ccf4444b343599f3dfdfc67f23df1d96c0108
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2024-02-17 13:10:21 +0100

    Update publications.tsx

--- src/data/publications.tsx
+++ src/data/publications.tsx
@@ -1,16 +1,16 @@
 export const PUBLICATIONS = [
   {
-    name: "Artificial Intelligence and Deep Learning for Advancing PET Image Reconstruction: State-of-the-Art and Future Directions",
-    authors: "Hellwig, D., Hellwig, N. C., Boehner, S., Fuchs, T., Fischer, R., & Schmidt, D. (2023).",
-    conference: "Nuklearmedizin-NuclearMedicine, 62(06), 334-342.",
-    url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10689088/",
-    tooltipUrl: "ncbi.nlm.nih.gov",
-  },
-  {
     name: "Transformer-Based Analysis of Sentiment Towards German Political Parties on Twitter During the 2021 Election Year",
     authors: "Hellwig, N. C., Bink, M., Schmidt, T., Fehle, J., & Wolff, C.",
     conference: "Proceedings of the 6th International Conference on Natural Language and Speech Processing (ICNLSP 2023)",
     url: "https://aclanthology.org/2023.icnlsp-1.9.pdf",
     tooltipUrl: "aclanthology.org",
   },
+  {
+    name: "Artificial Intelligence and Deep Learning for Advancing PET Image Reconstruction: State-of-the-Art and Future Directions",
+    authors: "Hellwig, D., Hellwig, N. C., Boehner, S., Fuchs, T., Fischer, R., & Schmidt, D. (2023).",
+    conference: "Nuklearmedizin-NuclearMedicine, 62(06), 334-342.",
+    url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10689088/",
+    tooltipUrl: "ncbi.nlm.nih.gov",
+  }
 ];
