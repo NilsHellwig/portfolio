14ee2b36911050a0981117e3bf46ce7e4cabbfc5
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2024-02-17 13:07:46 +0100

    Update publications.tsx

--- src/data/publications.tsx
+++ src/data/publications.tsx
@@ -6,4 +6,11 @@ export const PUBLICATIONS = [
     url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10689088/",
     tooltipUrl: "ncbi.nlm.nih.gov",
   },
+  {
+    name: "Transformer-Based Analysis of Sentiment Towards German Political Parties on Twitter During the 2021 Election Year",
+    authors: "Hellwig, N. C., Bink, M., Schmidt, T., Fehle, J., & Wolff, C.",
+    conference: "Proceedings of the 6th International Conference on Natural Language and Speech Processing (ICNLSP 2023)",
+    url: "https://aclanthology.org/2023.icnlsp-1.9.pdf",
+    tooltipUrl: "aclanthology.org",
+  },
 ];
