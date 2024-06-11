73c9bf16a0507452713ce79e6a59fd1b7d634995
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2024-06-11 08:20:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
       <Router>
         <Routes>
           <Route
-            path="portfolio/"
+            path="portfolio"
             element={
               <StandardUI transparentBackground={true}>
                 <Home />
