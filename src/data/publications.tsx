9b6d977f8d4778e75d8b5ea1d7fcdc0bb3c94ab1
NilsHellwig <nc.hellwig@gmail.com>
2024-11-01 16:31:51 +0100

    update publication list

--- src/data/publications.tsx
+++ src/data/publications.tsx
@@ -1,17 +1,13 @@
 export const PUBLICATIONS = [
   {
-    name: "Transformer-Based Analysis of Sentiment Towards German Political Parties on Twitter During the 2021 Election Year",
-    authors: "Hellwig, N. C., Bink, M., Schmidt, T., Fehle, J., & Wolff, C.",
-    conference: "Proceedings of the 6th International Conference on Natural Language and Speech Processing (ICNLSP 2023)",
-    url: "https://aclanthology.org/2023.icnlsp-1.9.pdf",
-    tooltipUrl: "aclanthology.org",
-  },
-  {
-    name: "Artificial Intelligence and Deep Learning for Advancing PET Image Reconstruction: State-of-the-Art and Future Directions",
-    authors: "Hellwig, D., Hellwig, N. C., Boehner, S., Fuchs, T., Fischer, R., & Schmidt, D. (2023).",
-    conference: "Nuklearmedizin-NuclearMedicine, 62(06), 334-342.",
-    url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10689088/",
-    tooltipUrl: "ncbi.nlm.nih.gov",
+    name: "Exploring large language models for the generation of synthetic training samples for aspect-based sentiment analysis in low resource settings",
+    authors: "Hellwig, Nils Constantin, Fehle, Jakob, & Wolff, Christian.",
+    journal: "Expert Systems with Applications",
+    pages: "125514",
+    year: 2024,
+    publisher: "Elsevier",
+    url: "https://www.sciencedirect.com/science/article/pii/S0957417424023819",
+    tooltipUrl: "sciencedirect.com",
   },
   {
     name: "GERestaurant: A German Dataset of Annotated Restaurant Reviews for Aspect-Based Sentiment Analysis",
@@ -33,13 +29,17 @@ export const PUBLICATIONS = [
     tooltipUrl: "springer.com",
   },
   {
-    name: "Exploring large language models for the generation of synthetic training samples for aspect-based sentiment analysis in low resource settings",
-    authors: "Hellwig, Nils Constantin, Fehle, Jakob, & Wolff, Christian.",
-    journal: "Expert Systems with Applications",
-    pages: "125514",
-    year: 2024,
-    publisher: "Elsevier",
-    url: "https://www.sciencedirect.com/science/article/pii/S0957417424023819",
-    tooltipUrl: "sciencedirect.com",
+    name: "Transformer-Based Analysis of Sentiment Towards German Political Parties on Twitter During the 2021 Election Year",
+    authors: "Hellwig, N. C., Bink, M., Schmidt, T., Fehle, J., & Wolff, C.",
+    conference: "Proceedings of the 6th International Conference on Natural Language and Speech Processing (ICNLSP 2023)",
+    url: "https://aclanthology.org/2023.icnlsp-1.9.pdf",
+    tooltipUrl: "aclanthology.org",
+  },
+  {
+    name: "Artificial Intelligence and Deep Learning for Advancing PET Image Reconstruction: State-of-the-Art and Future Directions",
+    authors: "Hellwig, D., Hellwig, N. C., Boehner, S., Fuchs, T., Fischer, R., & Schmidt, D. (2023).",
+    conference: "Nuklearmedizin-NuclearMedicine, 62(06), 334-342.",
+    url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10689088/",
+    tooltipUrl: "ncbi.nlm.nih.gov",
   }
 ];
